db40f570a3c25f4f6fecc4f2d8b7ffbfc8429de5
Bruno Santos <bruno.santos@bluecape.com.pt>
2016-09-25 20:44:02 +0100

    Updated the 'bin/updateDownStats.sh' script to only report on error.

--- bin/updateDownStats.sh
+++ bin/updateDownStats.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 # Based on: https://github.com/asstermo/asstermo.github.io/blob/master/bin/updateDownStats.sh
 #
 # crontab -e
@@ -8,15 +8,22 @@
 
 cd ${0%/*} || exit 1
 
-filename=../_includes/downstats.md
+export filename=../_includes/downstats.md
 
 ./updateDownloads.py > $filename
 
 if ! git diff --quiet --exit-code -- $filename; then
 
+(
   git add $filename && \
   git commit -q -m "Download stats updated automatically, based on Github API." && \
-  git pull -q && \
-  git push -q
+  git pull && \
+  git push
+) > log.git 2>&1
+
+if $? -ne 0
+then
+  cat log.git
+fi
 
 fi
