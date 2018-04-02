41390ab4310ba6377df62e59ec09175f09841ce7
Bruno Santos <bruno.santos@bluecape.com.pt>
2018-04-02 12:09:14 +0100

    Revised how the git pull is logged.

--- bin/updateDownStats.sh
+++ bin/updateDownStats.sh
@@ -8,23 +8,26 @@
 
 cd ${0%/*} || exit 1
 
-git pull || exit 1
-
-export filename=../_includes/downstats.md
-
-./updateDownloads.py > $filename
-
-if ! git diff --quiet --exit-code -- $filename; then
+git pull > log.git 2>&1 && \
 
 (
-  git add $filename && \
-  git commit -q -m "Download stats updated automatically, based on Github API." && \
-  git push
-) > log.git 2>&1
+
+  export filename=../_includes/downstats.md
+
+  ./updateDownloads.py > $filename
+
+  if ! git diff --quiet --exit-code -- $filename; then
+  (
+    git add $filename && \
+    git commit -q -m "Download stats updated automatically, based on Github API." && \
+    git push
+  ) >> log.git 2>&1
+  fi
+
+)
 
 if [ $? -ne 0 ]
 then
   cat log.git
 fi
 
-fi
